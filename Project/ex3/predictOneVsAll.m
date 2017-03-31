ea871f7976289f00c316fbd0c15985909e6c8d88
skag7599 <agarwal.shubham7599@gmail.com>
2017-04-01 01:57:01 +0530

    Update predictOneVsAll.m

--- Project/ex3/predictOneVsAll.m
+++ Project/ex3/predictOneVsAll.m
@@ -17,18 +17,7 @@ size X
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
-%
-% Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the 
-%       max element, for more information see 'help max'. If your examples 
-%       are in rows, then, you can use max(A, [], 2) to obtain the max 
-%       for each row.
-%       
+
 size X
 size all_theta
 
